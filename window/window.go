9bac5555190bb925a579e88bf7ce742ecdc620a3
akif999 <aki01nf28@gmail.com>
2017-10-09 23:45:09 +0900

    development proccess

--- window/window.go
+++ window/window.go
@@ -6,38 +6,47 @@ import (
 	termbox "github.com/nsf/termbox-go"
 )
 
+const (
+	InitialBufferSize = 4095
+)
+
 type Window struct {
 	lines  [][]rune
 	Buf    *buffer.Buffer
 	Cursor *cursor.Cursor
 }
 
-func NewWindow() *Window {
-	b := buffer.NewBuffer()
-	c := cursor.NewCursor()
-	return &Window{Buf: b, Cursor: c}
+type lines struct {
 }
 
-func (w *Window) BufToLines() {
-	var line []rune
-	for _, r := range w.Buf.Runes {
-		line = append(line, r)
-		if r == '\n' {
-			w.lines = append(w.lines, line)
-		}
-	}
+func NewWindow() *Window {
+	buf := make([][]rune, InitialBufferSize)
+	b := buffer.NewBuffer()
+	c := cursor.NewCursor()
+	return &Window{lines: buf, Buf: b, Cursor: c}
 }
 
 func (w *Window) LineToBuf() {
+copyloop:
 	for _, line := range w.lines {
 		for _, r := range line {
 			w.Buf.Runes = append(w.Buf.Runes, r)
+			if r == 0 {
+				break copyloop
+			}
 		}
+		w.Buf.Runes = append(w.Buf.Runes, '\n')
 	}
 }
 
 func (w *Window) Insert(chr rune) {
-	w.Buf.Insert(chr)
+	line := w.lines[w.Cursor.Y]
+	if len(line) > 1 {
+		line = append(line[:w.Cursor.X+1], line[w.Cursor.X:]...)
+		line[w.Cursor.X] = chr
+	} else {
+		line = append(line, chr)
+	}
 	w.Cursor.MoveRight()
 	if chr == '\n' {
 		w.Cursor.MoveHead()
@@ -46,6 +55,7 @@ func (w *Window) Insert(chr rune) {
 }
 
 func (w *Window) Draw() {
+	w.LineToBuf()
 	x, y := 0, 0
 	for _, r := range w.Buf.Runes {
 		termbox.SetCell(x, y, r, termbox.ColorWhite, termbox.ColorBlack)
