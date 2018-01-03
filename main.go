8b8e3fdfea2164e73ca3c4f0423cf01dc6107625
akif999 <aki01nf28@gmail.com>
2018-01-03 15:47:00 +0900

    implemant moving cursor

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+const (
+	Up = iota
+	Down
+	Left
+	Right
+)
+
 type buffer struct {
 	cursor cursor
 	lines  []*line
@@ -47,14 +54,22 @@ mainloop:
 		case termbox.EventKey:
 			switch ev.Key {
 			case termbox.KeyEnter:
+			case termbox.KeyArrowUp:
+				buf.moveCursor(Up)
+			case termbox.KeyArrowDown:
+				buf.moveCursor(Down)
+			case termbox.KeyArrowLeft:
+				buf.moveCursor(Left)
+			case termbox.KeyArrowRight:
+				buf.moveCursor(Right)
 			case termbox.KeyEsc:
 				break mainloop
 			default:
 				buf.insertChr(ev.Ch)
-				buf.updateLine()
-				buf.updateCursor()
 			}
 		}
+		buf.updateLine()
+		buf.updateCursor()
 		termbox.Flush()
 	}
 }
@@ -85,6 +100,40 @@ func (b *buffer) updateLine() {
 	}
 }
 
+func (b *buffer) moveCursor(d int) {
+	switch d {
+	case Up:
+		if b.cursor.y > 0 {
+			b.cursor.y--
+		}
+		break
+	case Down:
+		if b.cursor.y < b.linenum() {
+			b.cursor.y++
+		}
+		break
+	case Left:
+		if b.cursor.x > 0 {
+			b.cursor.x--
+		}
+		break
+	case Right:
+		if b.cursor.x < b.lines[b.cursor.y].runenum() {
+			b.cursor.x++
+		}
+		break
+	default:
+	}
+}
+
 func (b *buffer) updateCursor() {
 	termbox.SetCursor(b.cursor.x, b.cursor.y)
+}
+
+func (b *buffer) linenum() int {
+	return len(b.lines)
+}
+
+func (l *line) runenum() int {
+	return len(l.text)
 }
